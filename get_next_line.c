9a1b2c14f89e2aeaceea30bc9b437d67227854ce
Abdeljalil Bouchfar <bouchfar2000@gmail.com>
2021-11-17 14:01:06 +0100

    fix bugs && errors

---
+++ get_next_line.c
@@ -0,0 +1,92 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abouchfa <abouchfa@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/13 19:25:08 by abouchfa          #+#    #+#             */
+/*   Updated: 2021/11/17 13:51:16 by abouchfa         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+void	ft_free(void *p)
+{
+	if (p)
+		free(p);
+}
+
+void	reset_buffer(char *buffer)
+{
+	size_t	l;
+	size_t	i;
+
+	l = 0;
+	i = 0;
+	while (buffer[l] != '\n' && buffer[l] != '\0')
+		l++;
+	while (buffer[l++])
+		buffer[i++] = buffer[l];
+	buffer[i] = 0;
+}
+
+char	*collect_line(char *line)
+{
+	int		len;
+	char	*temp;
+
+	if (!*line)
+	{
+		ft_free(line);
+		return (NULL);
+	}
+	len = 0;
+	while (*(line + len) != '\n' && *(line + len) != '\0')
+		len++;
+	if (*(line + len) == '\n')
+		len++;
+	temp = line;
+	line = malloc (len + 1);
+	ft_strncpy(line, temp, len);
+	ft_free(temp);
+	return (line);
+}
+
+char	*read_line(int fd, char *buffer)
+{
+	char	*temp;
+	char	*line;
+	int		res;
+
+	res = 1;
+	line = NULL;
+	if (*buffer)
+	{
+		line = "\0";
+		line = ft_strjoin(line, buffer);
+	}
+	while (!gnl_strchr(line) && res > 0)
+	{
+		res = read(fd, buffer, BUFFER_SIZE);
+		buffer[res] = 0;
+		temp = line;
+		if (!line)
+			line = "\0";
+		line = ft_strjoin(line, buffer);
+		ft_free(temp);
+	}
+	return (line);
+}
+
+char	*get_next_line(int fd)
+{
+	static char		buffer[BUFFER_SIZE + 1];
+	char			*line;
+
+	line = read_line(fd, buffer);
+	line = collect_line(line);
+	reset_buffer(buffer);
+	return (line);
+}
---
+++ get_next_line_utils_bonus.c
@@ -0,0 +1,71 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils_bonus.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abouchfa <abouchfa@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/17 13:29:55 by abouchfa          #+#    #+#             */
+/*   Updated: 2021/11/17 13:29:56 by abouchfa         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	int		i;
+	int		j;
+	size_t	size;
+	char	*res;
+
+	if (!s1 || !s2)
+		return (NULL);
+	size = ft_strlen((char *) s1) + ft_strlen((char *) s2) + 1;
+	res = malloc(sizeof(char) * size);
+	if (!res)
+		return (NULL);
+	i = 0;
+	j = 0;
+	while (s1[j])
+		res[i++] = s1[j++];
+	j = 0;
+	while (s2[j])
+		res[i++] = s2[j++];
+	res[i] = 0;
+	return (res);
+}
+
+int	gnl_strchr(const char *s)
+{
+	if (s)
+	{
+		while (*s)
+		{
+			if (*s == '\n')
+				return (1);
+			s++;
+		}
+	}
+	return (0);
+}
+
+size_t	ft_strlen(const char *s)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
+
+void	ft_strncpy(char *dest, char *src, int size)
+{
+	int	i;
+
+	i = -1;
+	while (++i < size)
+		dest[i] = src[i];
+	dest[i] = 0;
+}
